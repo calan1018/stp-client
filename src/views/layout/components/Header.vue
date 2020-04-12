47197662ddddc97d12da72f9593b0b654abb3526
calan1018 <calan@naver.com>
2020-04-12 22:44:57 +0900

    20200412

--- src/views/layout/components/Header.vue
+++ src/views/layout/components/Header.vue
@@ -5,19 +5,40 @@
         <ul>
           <li class="current_page_item">
             <a href="/" style="padding: 0px;">
-              <img src="../../../images/logo.png" style="padding-right:500px;" />
+              <img
+                src="../../../images/logo.png"
+                style="padding-right:500px;"
+              />
             </a>
           </li>
           <li class="current_page_item">
-            <a href="/login" accesskey="1" title style="color:gray; font-size:13px;">로그인</a>
+            <a
+              href="/login"
+              accesskey="1"
+              title
+              style="color:gray; font-size:13px;"
+              >로그인</a
+            >
           </li>
           <li class="current_page_item" style="color:gray;">|</li>
           <li>
-            <a href="/member" accesskey="1" title style="color:gray; font-size:13px;">회원가입</a>
+            <a
+              href="/member"
+              accesskey="1"
+              title
+              style="color:gray; font-size:13px;"
+              >회원가입</a
+            >
           </li>
           <li class="current_page_item" style="color:gray;">|</li>
           <li>
-            <a href="/prototypePage" accesskey="1" title style="color:gray; font-size:13px;">프로토타입</a>
+            <a
+              href="/prototypePage"
+              accesskey="1"
+              title
+              style="color:gray; font-size:13px;"
+              >프로토타입</a
+            >
           </li>
         </ul>
       </div>
@@ -27,19 +48,23 @@
       <div id="menu" class="container">
         <ul>
           <li class="current_page_item">
-            <a href="/myInfo" accesskey="1" title>{{getMyInfoTitle}}</a>
+            <a href="/myInfo" accesskey="1" title>{{ getMyInfoTitle }}</a>
           </li>
           <li>
-            <a href="/accountInfo" accesskey="1" title>{{getAccountTitle}}</a>
+            <a href="/accountInfo" accesskey="1" title>{{ getAccountTitle }}</a>
           </li>
           <!-- <li>
             <a href="/invertInfo" accesskey="2" title>{{getInvestTitle}}</a>
           </li> -->
           <li>
-            <a href="/transactionInfo" accesskey="3" title>{{getTransactionTitle}}</a>
+            <a href="/transactionInfo" accesskey="3" title>{{
+              getTransactionTitle
+            }}</a>
           </li>
           <li>
-            <a href="/customerCenter" accesskey="4" title>{{getCustomerTitle}}</a>
+            <a href="/customerCenter" accesskey="4" title>{{
+              getCustomerTitle
+            }}</a>
           </li>
         </ul>
       </div>
@@ -67,5 +92,5 @@ export default {
       return this.$store.getters.getCustomerTitle;
     }
   }
-}
+};
 </script>
